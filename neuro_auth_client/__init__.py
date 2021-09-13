02d785e30008c17e2b0db7c63dea24877e55d358
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-09-13 20:24:59 +0300

    Export names

--- neuro_auth_client/__init__.py
+++ neuro_auth_client/__init__.py
@@ -3,23 +3,27 @@ from pkg_resources import get_distribution
 
 from .api import check_permissions
 from .client import (
+    Action,
     AuthClient,
     ClientAccessSubTreeView,
     ClientSubTreeViewRoot,
     Cluster,
     Permission,
     Quota,
+    Role,
     User,
 )
 
 
 __all__ = [
+    "Action",
     "AuthClient",
     "ClientAccessSubTreeView",
     "ClientSubTreeViewRoot",
     "Cluster",
     "Permission",
     "Quota",
+    "Role",
     "User",
     "check_permissions",
 ]
